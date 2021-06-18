6a959f4224a290f5adecf16f52b5102eb63755d9
gammabeta656 <gammabeta656@gmail.com>
2021-06-18 05:03:41 +0100

    salvemnos desse inferno pls

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -6,7 +6,15 @@ public class Delivery : MonoBehaviour
 {
     public List<GameObject> Boxes;
     public List<GameObject> Bag;
-    public float boxSize = 3.0f;
+    public float boxSize = 2.0f;
+    public float timer = 1.0f;
+    public bool recebeuCaixa;
+    public bool entregouCaixa;
+    public float timerMax = 1.0f;
+    public Collider deliveryVar;
+    public int index;
+    public GameObject caixaRef;
+    public GameObject playerPosition;
 
     public void StackBoxes()
     {
@@ -17,7 +25,7 @@ public class Delivery : MonoBehaviour
         if (Bag.Count > 0)
         {
             GameObject last = Bag[Bag.Count - 1];
-            gameobj.transform.localPosition = last.transform.localPosition + new Vector3(0, boxSize, 0);
+            gameobj.transform.localPosition = last.transform.localPosition + new Vector3(0, 1, 0);
         }
         else
         {
@@ -27,25 +35,72 @@ public class Delivery : MonoBehaviour
         Bag.Add(gameobj);
     }
 
+    private void Update()
+    {
+       if (recebeuCaixa == true || entregouCaixa == true)
+       {
+            timer -= Time.deltaTime;
+            Debug.Log(timer);
+        }
+       
+       if (timer <= 0)
+       {
+            EnableCollider(deliveryVar);
+            timer = timerMax;
+
+            if (recebeuCaixa)
+            {
+                recebeuCaixa = false;
+            }
+
+            if (entregouCaixa)
+            {
+                entregouCaixa = false;
+            }
+       }
+    }
+
     public void DestroyBoxes()
     {
+
+        Destroy(Bag[Bag.Count - 1]);
         Bag.RemoveAt(Bag.Count - 1);
     }
 
 
     public void OnTriggerEnter(Collider delivery)
     {
+        deliveryVar = delivery;
         if (delivery.gameObject.tag == "Restaurant")
         {
             StackBoxes();
             Debug.Log("Recebeu");
+
+            recebeuCaixa = true;
+
+            DisableCollider(deliveryVar);
         }
         else if (delivery.gameObject.tag == "Client")
         {
             DestroyBoxes();
             Debug.Log("Entregou");
+
+            entregouCaixa = true; 
+
+            DisableCollider(deliveryVar);
         }
+
+        
     }
 
+    public void EnableCollider(Collider delivery)
+    {
+        delivery.enabled = true;
+    }
 
+    public void DisableCollider(Collider delivery)
+    {
+        delivery.enabled = false;
+    }
 }
+
